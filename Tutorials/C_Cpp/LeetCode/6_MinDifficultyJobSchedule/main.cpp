87d9e832c581aff9a27783a919311a1d9fdf6ba9
w3rbl3r <4308316+w3rbl3r@users.noreply.github.com>
2022-06-05 11:27:29 -0500

    Moved sort and bst out of this repo, added min difficulty job scheduling problem from leetcode.

---
+++ Tutorials/C_Cpp/LeetCode/6_MinDifficultyJobSchedule/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+class Solution
+{
+public:
+    int minDifficulty(vector<int> &jobDifficulty, int d)
+    {
+    }
+};
